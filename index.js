e4b14093a6926635d904b2c784c8bef7adfeb693
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-09-26 10:10:36 +0600

    category news

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -15,6 +16,29 @@ app.get('/', (req, res) => {
 
 app.get('/categories',(req,res)=>{
     res.send(categories);
+})
+
+app.get('/news',(req,res)=>{
+  res.send(news);
+})
+
+app.get('/news/:id',(req,res)=>{
+  const id = req.params.id;
+  const selectedNews = news.find(n => n.id === id);
+  res.send(selectedNews);
+})
+
+app.get("/categories/:id",(req,res)=>{
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if(id === 0){
+    res.send(news)
+  }
+  else{
+    const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+  
 })
 
 app.listen(port, () => {
